e016fd50f25963f3657482f73358b86af6d0ff40
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-09-20 21:27:56 +0200

    add enums

---
+++ enums/index.ts
@@ -0,0 +1,18 @@
+export enum FilterableField {
+  Collectible = 'collectible',
+  CardSetId = 'cardSetId',
+  SpellSchoolId = 'spellSchoolId',
+  RarityId = 'rarityId',
+  CardTypeId = 'cardTypeId',
+  ManaCost = 'manaCost',
+  Attack = 'attack',
+  Health = 'health',
+  Armor = 'armor',
+}
+
+export enum NumericFields {
+  ManaCost = 'manaCost',
+  Attack = 'attack',
+  Health = 'health',
+  Armor = 'armor',
+}
