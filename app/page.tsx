c6fdc41b499b072d1ae1a844c6f28dd62051ebce
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-09-20 21:49:59 +0200

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
+import { useCallback, useEffect, useState } from 'react';
+import { useEffectOnce } from 'react-use';
+
+import { getAllCards } from '@/api/getAllCards';
+import { getAllClasses } from '@/api/getAllClasses';
+import { getToken } from '@/api/getToken';
+import { Card } from '@/components/Card';
+import { ClassBanner } from '@/components/ClassBanner';
+// import { Navbar } from '@/components/Navbar';
+import type { Card as CardType, Class } from '@/types';
+import { generateGhostCards } from '@/utils/generateGhostCards';
+import { getClassNameByClassId } from '@/utils/getClassNameByClassId';
+
+type CardsByClassId = {
+  classId: number;
+  groupOfCards: CardType[];
+};
+
+const Home = () => {
+  const [cards, setCards] = useState<CardType[]>([]);
+  const [page, setPage] = useState(1);
+  const [classes, setClasses] = useState<Class[]>([]);
+
+  const NUM_GHOST_CARDS = 5;
+  let globalCardsIndex = -1; // NOTE count global card index for each class array, to be able to detect the last one (it needs for infinity scroll)
+
+  const fetchToken = async () => {
+    await getToken();
+  };
+
+  const fetchClasses = async () => {
+    const classesData = await getAllClasses();
+    setClasses(classesData);
+  };
+
+  const fetchCards = useCallback(async () => {
+    const { cards: cardsData } = await getAllCards(page);
+    setCards((prev) => [...prev, ...cardsData]);
+  }, [page]);
+
+  useEffectOnce(() => {
+    fetchToken();
+    fetchClasses();
+  });
+
+  useEffect(() => {
+    fetchCards();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  // NOTE Split cards into groups based on classId
+  const splitCardsByClassId = () => {
+    const cardsByClassId: CardsByClassId[] = [];
+
+    cards.forEach((card) => {
+      const { classId } = card;
+      const existingEntry = cardsByClassId.find((entry) => entry.classId === classId);
+
+      if (!existingEntry) {
+        cardsByClassId.push({ classId, groupOfCards: [card] });
+      } else {
+        existingEntry.groupOfCards.push(card);
+      }
+    });
+
+    return cardsByClassId;
+  };
+
+  const cardsByClassId: CardsByClassId[] = splitCardsByClassId();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      {/* <Navbar /> */}
+
+      <div className="mx-auto max-w-[1600px] px-2.5">
+        <div className="flex flex-wrap justify-evenly">
+          {cardsByClassId.map(({ classId, groupOfCards }) => {
+            const className = getClassNameByClassId(classId, classes);
+
+            return (
+              <div key={classId}>
+                {className && <ClassBanner className={className} />}
+                <div className="flex flex-wrap justify-evenly">
+                  {groupOfCards.map(({ image, id, slug, name }) => {
+                    globalCardsIndex += 1;
+                    return (
+                      <Card
+                        key={id}
+                        slug={slug}
+                        imgSrc={image}
+                        alt={name}
+                        isLast={globalCardsIndex === cards.length - 1}
+                        newLimit={() => setPage(page + 1)}
+                      />
+                    );
+                  })}
+
+                  {generateGhostCards(NUM_GHOST_CARDS)}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
+    </>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
