f85ce792e6e9cd5fbd30d2d3574066f262dcfb49
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-09-22 22:03:01 +0200

    add props

--- api/getAllCards.ts
+++ api/getAllCards.ts
@@ -5,11 +5,15 @@ import type { CardList } from '@/types';
 const baseUrl = 'https://us.api.blizzard.com/hearthstone/cards';
 const PAGE_SIZE = 100;
 
-export const getAllCards = async (page: number): Promise<CardList> => {
+export const getAllCards = async (
+  page = 1,
+  set = 'standard',
+  heroClass = 'all'
+): Promise<CardList> => {
   try {
     const accessToken = localStorage.getItem('access_token');
 
-    const apiUrl = `${baseUrl}?locale=en_US&page=${page}&pageSize=${PAGE_SIZE}&set=standard&sort=manaCost:asc,name:asc,classes:asc,groupByClass:asc`;
+    const apiUrl = `${baseUrl}?class=${heroClass}&locale=en_US&page=${page}&pageSize=${PAGE_SIZE}&set=${set}&sort=manaCost:asc,name:asc,classes:asc,groupByClass:asc`;
 
     const response = await axios.get(apiUrl, {
       headers: {
