88f979fc1cb834a06cf42a8b8b1ec9aa537eac08
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-09-22 21:56:57 +0200

    add SVGR

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
