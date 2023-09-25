a48535b7b4e64e556b0071fe4344a4f085435cba
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-09-25 02:20:19 +0200

    add base query params

--- api/getAllCards.ts
+++ api/getAllCards.ts
@@ -5,15 +5,17 @@ import type { CardList } from '@/types';
 const baseUrl = 'https://us.api.blizzard.com/hearthstone/cards';
 const PAGE_SIZE = 100;
 
-export const getAllCards = async (
+export const getAllCards = async ({
   page = 1,
+  manaCost = '',
   set = 'standard',
-  heroClass = 'all'
-): Promise<CardList> => {
+  heroClass = 'all',
+  textFilter = '',
+}): Promise<CardList> => {
   try {
     const accessToken = localStorage.getItem('access_token');
 
-    const apiUrl = `${baseUrl}?class=${heroClass}&locale=en_US&page=${page}&pageSize=${PAGE_SIZE}&set=${set}&sort=manaCost:asc,name:asc,classes:asc,groupByClass:asc`;
+    const apiUrl = `${baseUrl}?class=${heroClass}&textFilter=${textFilter}&manaCost=${manaCost}&locale=en_US&page=${page}&pageSize=${PAGE_SIZE}&set=${set}&sort=manaCost:asc,name:asc,classes:asc,groupByClass:asc`;
 
     const response = await axios.get(apiUrl, {
       headers: {
