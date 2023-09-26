67fcf5f143002823ba960d2f5159e41b44e03163
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-09-26 21:51:14 +0200

    refactor styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export const Navbar: React.FC<Props> = ({ metadata, cardCount }) => {
   return (
-    <div className="overflow-hidden bg-[url(https://d2q63o9r0h0ohi.cloudfront.net/images/card-gallery/bg_filter_middle_tile-907853600f2b8dc11ae3acca94ff309f58b4959f380a6995e1b69b2d94f4bae5811b3ec588d56290599d59ddf6d00f0ce3fa9087c08ee8f7eabe843ed3646f9d.jpg)]">
-      <nav className=" flex">
+    <div className="relative overflow-hidden bg-[url(../public/bgFilterMiddleTile.jpeg)] pt-[11px] before:absolute before:top-0 before:h-[60px] before:w-full before:bg-[url(../public/bgFilterTopTile.png)] before:bg-top before:bg-repeat-x before:content-[''] after:absolute after:-bottom-[15px] after:h-[60px] after:w-full after:bg-[url(../public/bgFilterBottomTile.png)] after:bg-top after:bg-repeat-x after:content-['']">
+      <nav className="961:w-full 961:p-[0_5rem_0_2.5rem] relative z-[2] mx-auto flex h-[93px] w-[calc(90%_+_15px)] max-w-[1600px] items-center justify-center py-2.5">
         <FilterDrawer cardCount={cardCount} />
       </nav>
     </div>
