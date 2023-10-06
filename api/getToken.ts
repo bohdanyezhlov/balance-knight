2ff4983d33a5fca99c78f2be0f151da4f59e790e
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-10-06 23:03:02 +0200

    test refresh token

--- api/getToken.ts
+++ api/getToken.ts
@@ -6,8 +6,15 @@ const tokenEndpoint = process.env.NEXT_PUBLIC_TOKENENDPOINT;
 
 export const getToken = async (): Promise<string> => {
   const storedToken = localStorage.getItem('access_token');
-  if (storedToken) {
-    return storedToken;
+  const storedTokenExpiration = localStorage.getItem('token_expiration');
+
+  if (storedToken && storedTokenExpiration) {
+    const currentTime = Date.now();
+    const expirationTime = parseInt(storedTokenExpiration, 10);
+
+    if (currentTime < expirationTime) {
+      return storedToken;
+    }
   }
 
   const basicAuth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
@@ -22,7 +29,11 @@ export const getToken = async (): Promise<string> => {
   try {
     const response = await axios.post(tokenEndpoint!, data, { headers });
     const token = response.data.access_token;
+    const expiresIn = response.data.expires_in;
+    const expirationTime = Date.now() + expiresIn * 1000;
+
     localStorage.setItem('access_token', token);
+    localStorage.setItem('token_expiration', expirationTime.toString());
 
     return token;
   } catch (error) {
