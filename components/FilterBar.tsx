08118982170fb06324fa5ef0d90c229f5da8fd45
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-10-28 05:44:02 +0200

    add desktop drawer

--- components/FilterBar.tsx
+++ components/FilterBar.tsx
@@ -1,19 +1,34 @@
 'use client';
 
+import { useState } from 'react';
+
+import { useCardsContext } from '@/contexts/CardsContext';
 import { useMetadataContext } from '@/contexts/MetadataContext';
 import { useScreenSize } from '@/hooks/useScreenSize';
+import { cn } from '@/utils/cn';
 
 import { AttributeFilter } from './AttributeFilter';
 import { CardSet } from './CardSet';
 import { FilterDrawer } from './FilterDrawer';
 import { ManaCost } from './ManaCost';
 import { Search } from './Search';
+import { SwipeableDrawer } from './SwipeableDrawer';
 
 type Props = {};
 
 export const FilterBar: React.FC<Props> = () => {
   const metadata = useMetadataContext();
   const screenSize = useScreenSize();
+  const { cardCount } = useCardsContext()!; // REVIEW
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event && 'key' in event && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setIsOpen(open);
+  };
 
   if (!metadata) return null;
 
@@ -22,18 +37,57 @@ export const FilterBar: React.FC<Props> = () => {
       <div className="relative z-[2] mx-auto flex h-[93px] w-[calc(90%_+_15px)] max-w-[1600px] items-center justify-center py-2.5 md+:w-full md+:p-[0_20px_0_10px]">
         <CardSet metadata={metadata} />
 
-        {screenSize.width >= 960 && <AttributeFilter variant="class" hasIcon isDesktopView />}
+        {screenSize.width >= 960 && (
+          <AttributeFilter variant="class" hasIcon isDesktopView styles="mb-0" />
+        )}
 
         {screenSize.width >= 960 && screenSize.width < 1260 && (
-          <AttributeFilter variant="manaCost" hasIcon isDesktopView />
+          <AttributeFilter variant="manaCost" hasIcon isDesktopView styles="mb-0" />
         )}
 
         {screenSize.width > 1260 && <ManaCost />}
 
         <Search />
 
-        <FilterDrawer />
+        <FilterDrawer isOpen={isOpen} toggleDrawer={toggleDrawer} />
       </div>
+
+      {screenSize.width <= 960 ? (
+        <SwipeableDrawer isOpen={isOpen} cardCount={cardCount} toggleDrawer={toggleDrawer} />
+      ) : (
+        isOpen && (
+          <div
+            className={cn('relative h-0 min-h-0 transition-all delay-0 duration-200 ease-out', {
+              'h-auto min-h-[80px] border-t border-t-[#701b1a] pb-2.5 pl-2.5 pr-5 pt-[12px] before:absolute before:top-[-2px] before:h-px before:w-full before:bg-[#461310] before:content-[""]':
+                isOpen,
+            })}
+          >
+            <div className="mx-auto flex w-full max-w-[1600px] flex-wrap items-center transition-opacity delay-300 duration-100 ease-[ease]">
+              <AttributeFilter variant="attack" hasIcon isDesktopView />
+
+              <AttributeFilter variant="health" hasIcon isDesktopView />
+
+              <AttributeFilter variant="type" excludedIds={[10, 40]} isDesktopView />
+
+              <AttributeFilter
+                variant="minionType"
+                excludedIds={[1, 2, 3, 4, 6, 7, 8, 9, 10, 88, 93, 94, 95]}
+                isDesktopView
+              />
+
+              <AttributeFilter variant="spellSchool" isDesktopView />
+
+              <AttributeFilter variant="rarity" isDesktopView />
+
+              <AttributeFilter
+                variant="keyword"
+                excludedIds={[109, 196, 198, 234, 235, 252, 261]}
+                isDesktopView
+              />
+            </div>
+          </div>
+        )
+      )}
     </div>
   );
 };
--- components/FilterDrawer.tsx
+++ components/FilterDrawer.tsx
@@ -1,43 +1,26 @@
-import { useState } from 'react';
-
-import { useCardsContext } from '@/contexts/CardsContext';
-
 import SettingsIcon from '../public/settingsIcon.svg';
 import { BaseLayer } from './BaseLayer';
-import { SwipeableDrawer } from './SwipeableDrawer';
 import { TopLayerWithHover } from './TopLayerWithHover';
 
-type Props = {};
+type Props = {
+  isOpen: boolean;
+  toggleDrawer: (v: boolean) => React.ReactEventHandler<{}>;
+};
 
-export const FilterDrawer: React.FC<Props> = () => {
-  const { cardCount } = useCardsContext()!; // REVIEW
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-    if (event && 'key' in event && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setIsOpen(open);
-  };
-
+export const FilterDrawer: React.FC<Props> = ({ isOpen, toggleDrawer }) => {
   return (
-    <>
-      <div className="relative mr-[30px] flex justify-end md+:flex-1">
-        <BaseLayer>
-          <TopLayerWithHover as="button" onClick={toggleDrawer(true)}>
-            <div className="absolute left-[-5px] top-1.5 z-[101] h-[30px] w-[30px] fill-darkBrown">
-              <SettingsIcon />
-            </div>
-            <h6 className="mx-2.5 hidden font-serif text-[16px] font-bold text-mainBrown min-[1261px]:block">
-              Filters
-            </h6>
-          </TopLayerWithHover>
-        </BaseLayer>
-      </div>
+    <div className="relative mr-[30px] flex justify-end md+:flex-1">
+      <BaseLayer>
+        <TopLayerWithHover as="button" onClick={toggleDrawer(!isOpen)}>
+          <div className="absolute left-[-5px] top-1.5 z-[101] h-[30px] w-[30px] fill-darkBrown">
+            <SettingsIcon />
+          </div>
 
-      <SwipeableDrawer isOpen={isOpen} cardCount={cardCount} toggleDrawer={toggleDrawer} />
-    </>
+          <h6 className="mx-2.5 hidden font-serif text-[16px] font-bold text-mainBrown min-[1261px]:block">
+            Filters
+          </h6>
+        </TopLayerWithHover>
+      </BaseLayer>
+    </div>
   );
 };
