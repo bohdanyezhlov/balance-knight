37c784b4fdf14495701cfe0e022ed786883b9877
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-10-29 02:11:43 +0200

    add active filters

--- components/StatusBar.tsx
+++ components/StatusBar.tsx
@@ -3,10 +3,56 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import { useCardsContext } from '@/contexts/CardsContext';
+import { useMetadataContext } from '@/contexts/MetadataContext';
 import { useScreenSize } from '@/hooks/useScreenSize';
 import ClearAllFilters from '@/public/clearAllFilters.svg';
+import type { TMetadata } from '@/types';
 
+import { Checkbox } from './Checkbox';
 import { SortBy } from './SortBy';
+
+const normalizeActiveFilter = (param: string, value: string, metadata: TMetadata) => {
+  switch (param) {
+    case 'class': {
+      return metadata.classes.filter(({ slug }) => slug === value)[0].name;
+    }
+    case 'keyword': {
+      return metadata.keywords.filter(({ slug }) => slug === value)[0].name;
+    }
+    case 'spellSchool': {
+      return metadata.spellSchools.filter(({ slug }) => slug === value)[0].name;
+    }
+    case 'type': {
+      return metadata.types.filter(({ slug }) => slug === value)[0].name;
+    }
+    case 'minionType': {
+      return metadata.minionTypes.filter(({ slug }) => slug === value)[0].name;
+    }
+    case 'rarity': {
+      return metadata.rarities.filter(({ slug }) => slug === value)[0].name;
+    }
+    case 'manaCost': {
+      return `Mana: ${value}`;
+    }
+    case 'health': {
+      return `Health: ${value}`;
+    }
+    case 'attack': {
+      return `Attack: ${value}`;
+    }
+    case 'textFilter': {
+      return value;
+    }
+    case 'set': {
+      return `"${value
+        .split('-')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')}"`;
+    }
+    default:
+      throw new Error(`Unexpected param: ${param} or value: ${value}}`);
+  }
+};
 
 const preservedKeys = [
   'class',
@@ -25,13 +71,14 @@ type Props = {};
 
 export const StatusBar: React.FC<Props> = () => {
   const { cardCount } = useCardsContext()!; // FIXME
+  const metadata = useMetadataContext();
   const screenSize = useScreenSize();
   const searchParams = useSearchParams();
   const router = useRouter();
   const set = searchParams.get('set') || 'standard';
   const params = Array.from(searchParams).map(([param, value]) => ({ param, value }));
 
-  const activeFilters = params.filter((p) => preservedKeys.includes(p.param));
+  const activeFilters = params.filter((param) => preservedKeys.includes(param.param));
 
   const handleClearSingleParam = (param: string) => () => {
     const currentSearchParams = new URLSearchParams(searchParams.toString());
@@ -41,10 +88,9 @@ export const StatusBar: React.FC<Props> = () => {
 
   const handleClearAllParams = () => {
     const currentSearchParams = new URLSearchParams(searchParams.toString());
-
-    const newSearchParams = Array.from(currentSearchParams).reduce((acc, [key, value]) => {
+    const newSearchParams = Array.from(currentSearchParams).reduce((acc, [key]) => {
       if (preservedKeys.includes(key)) {
-        acc.set(key, value);
+        acc.delete(key);
       }
 
       return acc;
@@ -53,11 +99,17 @@ export const StatusBar: React.FC<Props> = () => {
     router.push(`?${newSearchParams.toString()}`);
   };
 
+  if (!metadata) return null;
+
   return (
     <div className="relative z-[2] pt-10">
       <div className="mx-auto flex max-w-[1600px] flex-wrap items-center justify-center px-5 xs:flex-nowrap">
-        <div className="flex flex-auto flex-wrap justify-start">
-          <div className="mr-2.5 font-bold text-mainBrown">{`${cardCount} cards found for ${set}`}</div>
+        <div className="flex flex-1 flex-wrap justify-start">
+          <div className="mr-2.5 font-bold text-mainBrown">{`${cardCount} cards found for ${normalizeActiveFilter(
+            'set',
+            set,
+            metadata
+          )}`}</div>
 
           {activeFilters.map(({ param, value }) => {
             return (
@@ -65,7 +117,7 @@ export const StatusBar: React.FC<Props> = () => {
                 key={value}
                 className="mb-2.5 mr-[5px] inline-flex rounded-[15px] bg-mainBrown px-2.5 py-[3px] text-[14px] text-white"
               >
-                {value}
+                {normalizeActiveFilter(param, value, metadata)}
                 <button
                   type="button"
                   className="ml-2.5 inline text-gold"
@@ -80,24 +132,31 @@ export const StatusBar: React.FC<Props> = () => {
           {activeFilters.length > 1 ? (
             <button
               type="button"
-              className="relative h-[25px] border-none bg-none px-10 py-[3px] text-[14px] text-[#233a6e]"
+              className="relative inline-flex h-[25px] border-none bg-none px-2.5 py-[3px] text-[14px] text-[#233a6e] before:absolute before:inset-0 before:z-[-1] before:rounded-[15px] before:border before:border-mainBrown before:bg-[#fff0da] before:opacity-50 before:content-[''] "
               onClick={handleClearAllParams}
             >
-              <div>
+              <div className="h-[18px] w-[18px] fill-[#233a6e]">
                 <ClearAllFilters />
               </div>
-              <span>Clear all</span>
+
+              <span>Clear All</span>
             </button>
           ) : null}
         </div>
 
         {screenSize.width > 960 && (
-          <div className="flex min-w-[200px] items-center">
+          <div className="mr-10 flex min-w-[200px] items-center">
             <div className="mr-2.5 text-mainBrown">Sort by:</div>
 
             <div className="mx-[25px]">
               <SortBy baseLayer={false} />
             </div>
+          </div>
+        )}
+
+        {screenSize.width > 960 && (
+          <div className="mr-[30px]">
+            <Checkbox labelStyle="text-mainBrown" />
           </div>
         )}
       </div>
