b90f2e8b3201f402375e445a738766e37805e294
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-10-29 19:40:00 +0100

    fix tw

--- utils/generateGhostCards.tsx
+++ utils/generateGhostCards.tsx
@@ -1,4 +1,4 @@
 export const generateGhostCards = (count: number) =>
   Array.from({ length: count }, (_, i) => (
-    <div key={`ghost_${i}`} className="h-[1px] w-[150px] min-[531px]:w-[240px]" />
+    <div key={`ghost_${i}`} className="h-px w-[150px] min-[531px]:w-[240px]" />
   ));
